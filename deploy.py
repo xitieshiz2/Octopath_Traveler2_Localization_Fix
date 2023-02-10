827e0aebbabe22eca23d77910956dac66772d6a7
xitieshiz2 <xitieshi@outlook.com>
2023-02-10 11:41:29 +0800

    初步修改字体，编写打包脚本，文本修改脚本待重写

--- deploy.py
+++ deploy.py
@@ -4,8 +4,8 @@ import sys
 
 
 
-TARGET_PATH = r'F:\SteamLibrary\steamapps\common\OCTOPATH TRAVELER\Octopath_Traveler_Localization_Fix\UnrealPakSwitch'
-OCTO_PATH = r'F:\SteamLibrary\steamapps\common\OCTOPATH TRAVELER\Octopath_Traveler\Content\Paks'
+TARGET_PATH = r'F:\SteamLibrary\steamapps\common\OCTOPATH TRAVELER\Octopath_Traveler_II_Localization_Fix\UnrealPakSwitch'
+OCTO_PATH = r'F:\SteamLibrary\steamapps\common\Octopath_Traveler2_DEMO\Octopath_Traveler2\Content\Paks'
 
 def main():
     scriptFolder = os.path.split(__file__)[0]
@@ -14,26 +14,41 @@ def main():
         for entry in it:
             if entry.is_file():
                 fileName, filePath = entry.name, entry.path
-                if fileName.startswith('TalkData_ZH_CH'):
-                    outputFolder = os.path.join(TARGET_PATH, r'Octopath_Traveler\Content\Talk\Database')
+                if fileName.startswith('TalkData_ZH_CN'):
+                    outputFolder = os.path.join(TARGET_PATH, r'Octopath_Traveler2\Content\Talk\Database')
                     if not os.path.exists(outputFolder):
                         os.makedirs(outputFolder)
                     shutil.copy2(filePath, outputFolder)
                 elif fileName.startswith('GameTextZH_CN'):
-                    outputFolder = os.path.join(TARGET_PATH, r'Octopath_Traveler\Content\GameText\Database')
+                    outputFolder = os.path.join(TARGET_PATH, r'Octopath_Traveler2\Content\GameText\Database')
+                    if not os.path.exists(outputFolder):
+                        os.makedirs(outputFolder)
+                    shutil.copy2(filePath, outputFolder)
+                elif fileName.startswith('NarrationTable_zh_cn'):
+                    outputFolder = os.path.join(TARGET_PATH, r'Octopath_Traveler2\Content\UserInterface\Narration\Database')
+                    if not os.path.exists(outputFolder):
+                        os.makedirs(outputFolder)
+                    shutil.copy2(filePath, outputFolder)
+                elif fileName.startswith('GameTextCharacterInfo_ZH_CN'):
+                    outputFolder = os.path.join(TARGET_PATH, r'Octopath_Traveler2\Content\Text\Database')
+                    if not os.path.exists(outputFolder):
+                        os.makedirs(outputFolder)
+                    shutil.copy2(filePath, outputFolder)
+                elif fileName.startswith('GameTextItemInfo_ZH_CN'):
+                    outputFolder = os.path.join(TARGET_PATH, r'Octopath_Traveler2\Content\Text\Database')
                     if not os.path.exists(outputFolder):
                         os.makedirs(outputFolder)
                     shutil.copy2(filePath, outputFolder)
                 elif fileName.endswith('.ttf'):
-                    outputFolder = os.path.join(TARGET_PATH, r'Octopath_Traveler\Content\UI\Font')
+                    outputFolder = os.path.join(TARGET_PATH, r'Octopath_Traveler2\Content\UserInterface\Common\Font')
                     if not os.path.exists(outputFolder):
                         os.makedirs(outputFolder)
                     shutil.copyfile(filePath, os.path.join(outputFolder, os.path.splitext(fileName)[0]) + '.ufont')
-    os.chdir(os.path.join(TARGET_PATH, r'v4\2\3'))
-    executeResult = os.popen(r'UnrealPak.exe ..\..\..\Octopath_Traveler-WindowsNoEditor_1_P.pak -Create=..\..\..\lista.txt -compress')
+    os.chdir(os.path.join(TARGET_PATH, r'v11.27\2\3'))
+    executeResult = os.popen(r'UnrealPak.exe ..\..\..\Octopath_Traveler2-WindowsNoEditor_cnfix.pak -Create=..\..\..\lista.txt -compress')
     for line in executeResult.read().splitlines():
         print(line)
-    shutil.copy2(os.path.join(TARGET_PATH, 'Octopath_Traveler-WindowsNoEditor_cnfix.pak'), OCTO_PATH)
+    shutil.copy2(os.path.join(TARGET_PATH, 'Octopath_Traveler2-WindowsNoEditor_cnfix.pak'), OCTO_PATH)
 
 if __name__ == '__main__':
     main()
